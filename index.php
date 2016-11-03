ee30e56d793132a1900733b9d72ceb6f3887258a
Wiryo Saputra <jagocode@gmail.com>
2016-11-03 07:16:08 +0000

    adding design for homepage

--- index.php
+++ index.php
@@ -24,6 +24,143 @@
        </div>
    </div>
    <!-- End Header -->
+   <!-- Promotion List -->
+   <div class="promotion-list">
+       
+       <div class="container">
+          
+           <div class="promotion-meta">
+                    <h3>Semua Promosi Tokopedia</h3>
+           <p>Bagi anda yang ingin berbelanja dengan harga terjangkau dengan kode promosi ataupun voucher tokopedia.</p>
+           </div>
+      
+           <div class="row">
+               
+               
+               <div class="col-md-4 col-sm-6">
+                    <a href="">
+               <div class="promotion-box">
+                       <div class="promotion-image">
+                           <img src="https://pulsa.tokopedia.com/blog/wp-content/uploads/2016/11/pulsa-blog-1200x630-1-750x410.jpg" class="img-responsive"></img>
+                       </div>
+                       <div class="promotion-description">
+                           <p>Dapatkan <strong>Diskon 20 Ribu Untuk Tiket Kereta Api</strong> dengan membeli pulsa di tokopedia</p>
+                          
+                           <div class="promotion-code">
+                               
+                               CEK SEKARANG
+                           </div>
+                       </div>
+                   </div>
+               
+               
+           </a>
+                   
+               </div>
+               <div class="col-md-4 col-sm-6">
+                    <a href="">
+               <div class="promotion-box">
+                       <div class="promotion-image">
+                           <img src="https://pulsa.tokopedia.com/blog/wp-content/uploads/2016/10/blog-pulsa-1200x630-1-750x410.jpg" class="img-responsive"></img>
+                       </div>
+                       <div class="promotion-description">
+                           <p>Beli Token Listrik di tokopedia.com <strong>Bebas Biaya Admin</strong></p>
+                          
+                           <div class="promotion-code">
+                               
+                               CEK SEKARANG
+                           </div>
+                       </div>
+                   </div>
+               
+               
+           </a>
+                   
+               </div><div class="col-md-4 col-sm-6">
+                    <a href="">
+               <div class="promotion-box">
+                       <div class="promotion-image">
+                           <img src="https://pulsa.tokopedia.com/blog/wp-content/uploads/2016/10/blog-pulsa-1200x630-4-1-750x410.jpg" class="img-responsive"></img>
+                       </div>
+                       <div class="promotion-description">
+                           <p>Beli Pulsa <strong>Dapat Potongan 30% Domino Pizza</strong> </p>
+                          
+                           <div class="promotion-code">
+                               
+                               CEK SEKARANG
+                           </div>
+                       </div>
+                   </div>
+               
+               
+           </a>
+                   
+               </div>
+               
+               <!-- Ke dua -->
+               <div class="col-md-4 col-sm-6">
+                    <a href="">
+               <div class="promotion-box">
+                       <div class="promotion-image">
+                           <img src="https://pulsa.tokopedia.com/blog/wp-content/uploads/2016/10/pulsa-blog-1200x630-1-750x410.jpg" class="img-responsive"></img>
+                       </div>
+                       <div class="promotion-description">
+                           <p>Dapatkan <strong>Diskon 20 Ribu Untuk Tiket Kereta Api</strong> dengan membeli pulsa di tokopedia</p>
+                          
+                           <div class="promotion-code">
+                               
+                               CEK SEKARANG
+                           </div>
+                       </div>
+                   </div>
+               
+               
+           </a>
+                   
+               </div>
+               <div class="col-md-4 col-sm-6">
+                    <a href="">
+               <div class="promotion-box">
+                       <div class="promotion-image">
+                           <img src="https://pulsa.tokopedia.com/blog/wp-content/uploads/2016/10/blog-1200x630-3-750x410.jpg" class="img-responsive"></img>
+                       </div>
+                       <div class="promotion-description">
+                           <p>Beli Token Listrik di tokopedia.com <strong>Bebas Biaya Admin</strong></p>
+                          
+                           <div class="promotion-code">
+                               
+                               CEK SEKARANG
+                           </div>
+                       </div>
+                   </div>
+               
+               
+           </a>
+                   
+               </div><div class="col-md-4 col-sm-6">
+                    <a href="">
+               <div class="promotion-box">
+                       <div class="promotion-image">
+                           <img src="https://pulsa.tokopedia.com/blog/wp-content/uploads/2016/10/pulsa-blog-1200x630-750x410.jpg" class="img-responsive"></img>
+                       </div>
+                       <div class="promotion-description">
+                           <p>Beli Pulsa <strong>Dapat Potongan 30% Domino Pizza</strong> </p>
+                          
+                           <div class="promotion-code">
+                               
+                               CEK SEKARANG
+                           </div>
+                       </div>
+                   </div>
+               
+               
+           </a>
+                   
+               </div>
+           </div>
+       </div>
+   </div>
+   <!-- End Promotion List -->
 
    <?php wp_footer();?>
   </body>
